a0ccb575bf4cf50fa947666507935d53a620d3af
Youyoun <belkouchiyounes@gmail.com>
2022-12-06 12:29:07 +0100

    Completed typings of blur module

--- toolbox/imageOperators/blur/blur_operator.py
+++ toolbox/imageOperators/blur/blur_operator.py
@@ -1,6 +1,6 @@
 import enum
 from pathlib import Path
-from typing import Union
+from typing import Union, Tuple
 
 import numpy as np
 from scipy.ndimage import gaussian_filter
@@ -46,13 +46,13 @@ class Operator:
     def __init__(self, *args, **kwargs):
         pass
 
-    def matvec(self, x):
+    def matvec(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplemented()
 
-    def rmatvec(self, x):
+    def rmatvec(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplemented()
 
-    def __matmul__(self, x):
+    def __matmul__(self, x: torch.Tensor) -> torch.Tensor:
         return self.matvec(x)
 
     def transpose(self):
@@ -62,23 +62,23 @@ class Operator:
 
 
 class TransposedOperator(Operator):
-    def __init__(self, operator):
+    def __init__(self, operator: Operator):
         super().__init__()
         self.op = operator
 
-    def __matmul__(self, x):
+    def __matmul__(self, x: torch.Tensor) -> torch.Tensor:
         return self.op.rmatvec(x)
 
 
 class IdentityOperator(Operator):
-    def matvec(self, x):
+    def matvec(self, x: torch.Tensor) -> torch.Tensor:
         return x
 
-    def rmatvec(self, x):
+    def rmatvec(self, x: torch.Tensor) -> torch.Tensor:
         return x
 
 
-def get_kernel_fft(kernel, dimensions):
+def get_kernel_fft(kernel: torch.Tensor, dimensions: Tuple[int, int, int, int]) -> torch.Tensor:
     kernel_h = F.pad(kernel, (dimensions[3] // 2, dimensions[3] // 2, dimensions[2] // 2, dimensions[2] // 2),
                      'constant')
     kernel_h = torch.fft.ifftshift(kernel_h)
@@ -86,7 +86,7 @@ def get_kernel_fft(kernel, dimensions):
     return torch.fft.fft2(kernel_h)
 
 
-def expand_x_dims(x):
+def expand_x_dims(x: torch.Tensor) -> Tuple[torch.Tensor, torch.Size]:
     init_shape = x.shape
     if x.ndim == 2:
         x = x.view(1, 1, *x.shape)
@@ -98,7 +98,7 @@ def expand_x_dims(x):
 class GaussianBlurFFT(Operator):
     PAD_MODE = "replicate"
 
-    def __init__(self, ksize, s=0.5):
+    def __init__(self, ksize: int, s: float = 0.5):
         super().__init__()
         # Define Gaussian Kernel
         self.kernel_size = ksize
@@ -106,9 +106,9 @@ class GaussianBlurFFT(Operator):
         self.kernel = get_kernel(ksize, Kernels.GAUSSIAN, self.std)
         self.matmul_func = self.matvec
 
-    def fft_convolve(self, x, conj: bool):
+    def fft_convolve(self, x: torch.Tensor, conj: bool) -> torch.Tensor:
         padding = self.kernel_size // 2
-        fft_h = get_kernel_fft(self.kernel.to(x.device), x.shape, padding)
+        fft_h = get_kernel_fft(self.kernel.to(x.device), x.shape)
         x_pad = torch.nn.functional.pad(x, (padding, padding, padding, padding), self.PAD_MODE)
         fft_x = torch.fft.fft2(x_pad)
         if conj:
@@ -117,19 +117,17 @@ class GaussianBlurFFT(Operator):
             x_new = torch.real(torch.fft.ifft2(fft_x * fft_h))
         return x_new
 
-    def blurr(self, x, conj: bool):
+    def blurr(self, x: torch.Tensor, conj: bool) -> torch.Tensor:
         x, init_shape = expand_x_dims(x)
         padding = self.kernel_size // 2
         x_new = self.fft_convolve(x, conj)
         x_new = x_new[:, :, padding:- padding, padding:-padding]
         return x_new.view(*init_shape)
 
-    def matvec(self, x):
-        #         print("Calling matvec")
+    def matvec(self, x: torch.Tensor) -> torch.Tensor:
         return self.blurr(x, False)
 
-    def rmatvec(self, x):
-        #         print("Calling rmatvec")
+    def rmatvec(self, x: torch.Tensor) -> torch.Tensor:
         return self.blurr(x, True)
 
 
@@ -149,42 +147,42 @@ class BlurConvolution(Operator):
             DID_LOG_ONCE = True
         # assert self.kernel.sum() == 1.00
 
-    def blurr(self, x, kernel):
+    def blurr(self, x: torch.Tensor, kernel: torch.Tensor) -> torch.Tensor:
         x, init_shape = expand_x_dims(x)
         padding = self.kernel_size // 2
         x_padded = F.pad(x, (padding, padding, padding, padding), self.PAD_MODE)
         x_blurred = F.conv2d(x_padded, kernel.to(x.device), bias=None, padding=0)
         return x_blurred.view(init_shape)
 
-    def matvec(self, x):
+    def matvec(self, x: torch.Tensor) -> torch.Tensor:
         return self.blurr(x, torch.flip(self.kernel, [0, 1]).view(1, 1, self.kernel_size, self.kernel_size))
 
-    def rmatvec(self, x):
+    def rmatvec(self, x: torch.Tensor) -> torch.Tensor:
         return self.blurr(x, self.kernel.view(1, 1, self.kernel_size, self.kernel_size))
 
 
 class ZeroOperator(Operator):
-    def matvec(self, x):
+    def matvec(self, x: torch.Tensor) -> torch.Tensor:
         return torch.zeros_like(x)
 
-    def rmatvec(self, x):
+    def rmatvec(self, x: torch.Tensor) -> torch.Tensor:
         return torch.zeros_like(x)
 
 
-def _convert_str_type_to_kernel(str_: str):
+def _convert_str_type_to_kernel(str_: str) -> Kernels:
     for ker in Kernels:
         if ker == str_:
             return ker
     raise ValueError(f"Kernel provided is not available: {str_}")
 
 
-def get_kernel(ksize: int, type_: Union[Kernels, str], sigma: float):
+def get_kernel(ksize: int, type_: Union[Kernels, str], sigma: float) -> torch.Tensor:
     if type(type_) == str:
         type_ = _convert_str_type_to_kernel(type_)
     if type_ == Kernels.GAUSSIAN:
         kernel = np.zeros(shape=(ksize, ksize))
         kernel[ksize // 2, ksize // 2] = 1
-        return torch.from_numpy(gaussian_filter(kernel, sigma)).float()  ## already normalized
+        return torch.from_numpy(gaussian_filter(kernel, sigma)).float()  # already normalized
     elif type_ == Kernels.UNIFORM:
         return torch.ones((ksize, ksize)) / ksize ** 2
     else:
