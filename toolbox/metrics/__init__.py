542f7cd6e940344a3ee12f1aa2fb4b1e8dfd61ff
Youyoun <belkouchiyounes@gmail.com>
2023-02-13 16:25:32 +0100

    Added compute_metrics functions to __init__

--- toolbox/metrics/__init__.py
+++ toolbox/metrics/__init__.py
@@ -1,1 +1,3 @@
 from .metrics import AverageMetricsDictionary, MetricAverage, MetricsDictionary, MetricArray
+from .compute_metrics import MetricNames, mean_squared_error, mean_absolute_error, compute_relative_difference, PSNR, \
+    SNR, SSIM
