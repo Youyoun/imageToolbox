1d6bc4d8a0a769af8c6b927306cd72cd87a558db
Younes Belkouchi <belkouchiyounes@gmail.com>
2023-02-20 11:54:53 +0100

    Implemented a load to memory for heavy computation functions on generic datasets

--- toolbox/dataset/generic_dataset.py
+++ toolbox/dataset/generic_dataset.py
@@ -17,23 +17,34 @@ class GenericDataset(data.Dataset):
                  n_images: int,
                  clean_transform_fn: Callable,
                  is_train: bool = True,
-                 augments: List[Tuple[Union[AvailableTransforms, str], Dict]] = None):
+                 augments: List[Tuple[Union[AvailableTransforms, str], Dict]] = None,
+                 load_in_memory: bool = False):
         self.data_path = Path(root)
         self.n_images = n_images
         self.is_train = is_train
         self.im_path = glob.glob(str(self.data_path / "*.jpg"))
         if n_images > 0:
             self.im_path = self.im_path[: self.n_images]
+        self.images = None
+        self.noisy_images = None
 
         self.transform_fn = clean_transform_fn
 
         self.transforms = get_transforms(augments)
         logger.info(f"Using Transforms: {self.transforms}")
-        return
+
+        self.load_in_memory = load_in_memory
+        if self.load_in_memory:
+            logger.info("Loading images in memory")
+            self.images = [get_clean_image(im_path)[1] for im_path in self.im_path]
+            self.noisy_images = [self.transform_fn(im) for im in self.images]
 
     def __getitem__(self, item):
-        _, clean_im = get_clean_image(self.im_path[item])
-        noisy_im = self.transform_fn(clean_im)
+        if self.load_in_memory:
+            clean_im, noisy_im = self.images[item], self.noisy_images[item]
+        else:
+            _, clean_im = get_clean_image(self.im_path[item])
+            noisy_im = self.transform_fn(clean_im)
         if self.transforms is not None:
             return self.transforms(clean_im, noisy_im)
         else:
