dd5e318096236d3d84cfa8428154fdfd8ab82469
Younes Belkouchi <belkouchiyounes@gmail.com>
2023-03-27 13:10:52 +0200

    added modules to __init__.py in dataset

--- toolbox/dataset/__init__.py
+++ toolbox/dataset/__init__.py
@@ -1,4 +1,7 @@
 from .BSD300 import BSD300PatchedNoisyDataset
+from .generic_dataset import GenericDataset
 from .image_splitter import ImageSplitter, ImageSplitterOverlapIgnored
+from .lmdb_generic_dataset import GenericLMDBDataset
+from .lmdb_utils import folder2lmdb
 from .transforms import get_transforms, AvailableTransforms, Compose, TransformsFactory, Random90Rotation, \
     RandomHorizontalFlip, RandomVerticalFlip, RandomCrop, CenterCrop
