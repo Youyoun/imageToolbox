294a618e2f41a8be6d569b640cba39604020bce8
Younes Belkouchi <belkouchiyounes@gmail.com>
2023-03-31 16:48:56 +0200

    Make convs pad with zeros

--- toolbox/models/utils.py
+++ toolbox/models/utils.py
@@ -9,6 +9,7 @@ from .activations import get_activation, Activation
 PADDING = 1
 STRIDE = 1
 KERNEL_SIZE = 3
+PADDING_MODE = "zeros"
 
 logger = get_module_logger(__name__)
 
@@ -22,20 +23,20 @@ def get_model(in_c: int,
               use_batchnorm: bool = False) -> nn.Module:
     if n_layers == 1:
         model = nn.Sequential(
-            nn.Conv2d(in_c, out_c, KERNEL_SIZE, STRIDE, PADDING, bias=True, padding_mode='replicate'),
+            nn.Conv2d(in_c, out_c, KERNEL_SIZE, STRIDE, PADDING, bias=True, padding_mode=PADDING_MODE),
             get_activation(last_activation)
         )
     else:
         layers = [
-            nn.Conv2d(in_c, mid_c, KERNEL_SIZE, STRIDE, PADDING, bias=True, padding_mode='replicate'),
+            nn.Conv2d(in_c, mid_c, KERNEL_SIZE, STRIDE, PADDING, bias=True, padding_mode=PADDING_MODE),
             get_activation(mid_activation)
         ]
         for i in range(1, n_layers - 1):
-            layers.append(nn.Conv2d(mid_c, mid_c, KERNEL_SIZE, STRIDE, PADDING, bias=False, padding_mode='replicate'))
+            layers.append(nn.Conv2d(mid_c, mid_c, KERNEL_SIZE, STRIDE, PADDING, bias=False, padding_mode=PADDING_MODE))
             if use_batchnorm:
                 layers.append(nn.BatchNorm2d(mid_c))
             layers.append(get_activation(mid_activation))
-        layers.append(nn.Conv2d(mid_c, out_c, KERNEL_SIZE, STRIDE, PADDING, bias=False, padding_mode='replicate'))
+        layers.append(nn.Conv2d(mid_c, out_c, KERNEL_SIZE, STRIDE, PADDING, bias=False, padding_mode=PADDING_MODE))
         layers.append(get_activation(last_activation))
         model = nn.Sequential(*layers)
     _initialize_weights(model)
