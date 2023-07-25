a4289d9a21d4e4e20bcceb337c87933c378a249b
Younes Belkouchi <belkouchiyounes@gmail.com>
2023-07-25 10:59:09 +0200

    generic_dataset: removed unused vars and added docstring + small refactor

--- toolbox/dataset/generic_dataset.py
+++ toolbox/dataset/generic_dataset.py
@@ -13,23 +13,34 @@ logger = get_module_logger(__name__)
 
 
 class GenericDataset(data.Dataset):
+    """
+    Loads images from a folder and applies a certain transformation to them (add noise, blur image, etc.).
+    The images provided are in the range 0 and 1.
+
+    :param root: Path to the folder containing the images.
+    :param n_images: Number of images to load. If 0, all images are loaded.
+    :param clean_transform_fn: Function to apply to the clean images.
+    :param augments: List of tuples (transform, kwargs) to apply to the images.
+    :param load_in_memory: If True, the images are loaded in memory.
+    :param colorized: If True, the images are loaded in color.
+    """
+
     def __init__(
         self,
         root: Union[str, Path],
         n_images: int,
         clean_transform_fn: Callable,
-        is_train: bool = True,
         augments: List[Tuple[Union[AvailableTransforms, str], Dict]] = None,
         load_in_memory: bool = False,
         colorized: bool = False,
     ):
         self.data_path = Path(root)
         self.n_images = n_images
-        self.is_train = is_train
         self.im_path = glob.glob(str(self.data_path / "*.jpg")) + glob.glob(
             str(self.data_path / "*.JPEG")
         )
         if n_images > 0:
+            logger.info(f"Loading {n_images} images")
             self.im_path = self.im_path[: self.n_images]
         self.images = None
         self.noisy_images = None
@@ -52,13 +63,17 @@ class GenericDataset(data.Dataset):
     def __getitem__(self, item):
         if self.load_in_memory:
             clean_im, noisy_im = self.images[item], self.noisy_images[item]
+            if self.transforms is not None:
+                return self.transforms(clean_im, noisy_im)
         else:
             _, clean_im = get_clean_image(self.im_path[item], not self.colorized)
-            noisy_im = self.transform_fn(clean_im)
-        if self.transforms is not None:
-            return self.transforms(clean_im, noisy_im)
-        else:
-            return clean_im, noisy_im
+            if self.transforms is not None:
+                return self.transforms(clean_im, self.transform_fn(clean_im))
+            else:
+                return clean_im, self.transform_fn(clean_im)
+
+    def get_image_path(self, item):
+        return self.im_path[item]
 
     def __len__(self):
         return len(self.im_path)
